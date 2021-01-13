99a96bde5aa22b86d2f031ed4b4a1e1766622b04
botekchristophe <botekchristophe@gmail.com>
2021-01-13 15:47:11 -0500

    feat: add variant index template

--- src/main/scala/org/kidsfirstdrc/variant/Indexer.scala
+++ src/main/scala/org/kidsfirstdrc/variant/Indexer.scala
@@ -4,13 +4,24 @@ import org.apache.spark.sql.functions.{concat, sha1}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.elasticsearch.spark.sql._
 
+import scala.util.Try
+
 object Indexer extends App {
 
-  val Array(input, esNodes, indexName, release) = args
+  val Array(input, esNodes, indexName, release, templateFileName) = args
   implicit val spark: SparkSession = SparkSession.builder
     .config("es.index.auto.create", "true")
     .config("es.nodes", esNodes)
+    .config("es.wan.only", true)
     .appName(s"Indexer").getOrCreate()
+
+  def setTemplate(esNodes: String, templateFileName: String): Unit = {
+    Try {
+      val esClient = new ElasticSearchClient(esNodes.split(',').head)
+      val response = esClient.setTemplate(templateFileName, templateFileName.split('.').head)
+      println(response.getStatusLine.getStatusCode + " : " + response.getStatusLine.getReasonPhrase)
+    }
+  }
 
   def run(df: DataFrame, indexName: String): Unit = {
     import spark.implicits._
@@ -25,6 +36,7 @@ object Indexer extends App {
     dfWithId.saveToEs(s"$indexName/_doc", Map("es.mapping.id" -> "id"))
   }
 
+  setTemplate(esNodes, templateFileName)
   run(spark.read.json(input), s"${indexName}_$release")
 
 
