b4b151b546bd652d69cea528b02bd85a92639fc7
botekchristophe <botekchristophe@gmail.com>
2021-02-12 09:27:45 -0500

    fix: now uses variant_index_template when pushing to es7

--- src/test/scala/org/kidsfirstdrc/ElasticSearchClientSpec.scala
+++ src/test/scala/org/kidsfirstdrc/ElasticSearchClientSpec.scala
@@ -7,11 +7,12 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
+import scala.util.Random
+
 class ElasticSearchClientSpec extends AnyFlatSpec with GivenWhenThen with WithSparkSession with Matchers {
 
-  val indexName = "test"
-  val templateFileName = "template.json"
-  val templateName = "test_template"
+  val indexName = Random.nextString(20)
+  val templateFileName = getClass.getClassLoader.getResource("template_example.json").getFile
   val esUrl = "http://localhost:9200"
   //qa testing: val esUrl = "https://vpc-kf-arranger-blue-es-service-exwupkrf4dyupg24dnfmvzcwri.us-east-1.es.amazonaws.com:443"
   val esClient = new ElasticSearchClient(esUrl)
@@ -20,20 +21,14 @@ class ElasticSearchClientSpec extends AnyFlatSpec with GivenWhenThen with WithSp
     esClient.isRunning shouldBe true
   }
 
-  "ES client" should "create index" in {
+  "ES client" should "create then delete index" in {
     esClient.createIndex(indexName).getStatusLine.getStatusCode shouldBe 200
-  }
-
-  "ES client" should "delete index" in {
     esClient.deleteIndex(indexName).getStatusLine.getStatusCode shouldBe 200
   }
 
-  "ES client" should "create template" in {
-    esClient.setTemplate(templateFileName, templateName).getStatusLine.getStatusCode shouldBe 200
-  }
-
-  "ES client" should "delete template" in {
-    esClient.deleteTemplate(templateName).getStatusLine.getStatusCode shouldBe 200
+  "ES client" should "create then delete template" in {
+    esClient.setTemplate(templateFileName).getStatusLine.getStatusCode shouldBe 200
+    esClient.deleteTemplate("template_example").getStatusLine.getStatusCode shouldBe 200
   }
 
 }
