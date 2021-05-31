6e0f8ded17a9ee81c2b2bfcd0b2219e2ee5efeee
cbotek <botekchristophe@gmail.com>
2021-05-31 15:22:22 -0400

    Add jenkins file

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+mkdir -p ~/.ivy2 ~/.sbt ~/.m2 ~/.sbt_cache
+docker run --rm -v $(pwd):/app/build \
+    --user $(id -u):$(id -g) \
+    -v ~/.m2:/app/.m2 \
+    -v ~/.ivy2:/app/.ivy2 \
+    -v ~/.sbt:/app/.sbt \
+    -v ~/.sbt_cache:/app/.cache \
+    -w /app/build hseeberger/scala-sbt:8u282_1.5.2_2.11.12 \
+    sbt -Duser.home=/app clean compile
